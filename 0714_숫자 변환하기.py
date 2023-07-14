c3d1bafd6ad7e1661c39179722a0b46ec334c146
hmuri <113418916+hmuri@users.noreply.github.com>
2023-07-14 23:40:37 +0900

    [Lv2] 숫자 변환하기

---
+++ 0714_숫자 변환하기.py
@@ -0,0 +1,15 @@
+def solution(x: int, y: int, n: int) -> int:
+    DP = [float('inf') for _ in range(y + 1)]
+    DP[x] = 0
+
+    for i in range(x, y + 1):
+        if DP[i] == float('inf'):
+            continue
+        if i + n <= y:
+            DP[i + n] = min(DP[i + n], DP[i] + 1)
+        if i * 2 <= y:
+            DP[i * 2] = min(DP[i * 2], DP[i] + 1)
+        if i * 3 <= y:
+            DP[i * 3] = min(DP[i * 3], DP[i] + 1)
+
+    return DP[y] if DP[y] != float('inf') else -1
